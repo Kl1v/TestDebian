010b4a557e672ec588b981b8e656638f0555f6e8
Laurenz Pichler <l.pichler@htlkrems.at>
2024-11-16 17:04:49 +0100

    Test

--- connection.php
+++ connection.php
@@ -1,20 +1,18 @@
 <?php
-$host = '193.154.207.221';    // IP-Adresse oder Domain des MariaDB-Servers
-$dbname = 'test';       // Der Name der Datenbank
-$username = 'kremsguesser';  // Der Benutzername
-$password = '123mysql';      // Das Passwort
+$servername = "193.154.207.221";
+$username = "kremsguesser";
+$password = "123mysql"; // Ersetze xxx mit deinem Passwort
+$dbname = "test";
 
-try {
-    // PDO-Verbindung aufbauen
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";  // DSN-String für MySQL/MariaDB
-    $pdo = new PDO($dsn, $username, $password);
+// Pfad zum SSL-Zertifikat
 
-    // Setze den PDO-Fehlermodus auf Exception, damit Fehler besser gehandhabt werden
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Initialisiere MySQLi
+$conn = mysqli_init();
 
-    echo "Verbindung erfolgreich!";
-} catch (PDOException $e) {
-    // Fehlerbehandlung
-    echo "Verbindung fehlgeschlagen: " . $e->getMessage();
+// SSL-Einstellungen setzen
+
+// Verbinde ohne Zertifikatsprüfung
+if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
+    die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
 }
 ?>
