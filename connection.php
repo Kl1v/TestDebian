6ece5b0533f573b10c2b32d399b407e8283ba1c0
Laurenz Pichler <l.pichler@htlkrems.at>
2025-01-16 12:07:26 +0100

    Test

--- connection.php
+++ connection.php
@@ -1,18 +1,16 @@
 <?php
-$servername = "194.155.88.76";
-$username = "kremsguesser";
-$password = "123mysql"; // Ersetze xxx mit deinem Passwort
-$dbname = "tdot";
+$servername = "kremsguesser.duckdns.org"; // Serveradresse
+$username = "kremsguesser";              // Benutzername
+$password = "123mysql";                  // Passwort
+$dbname = "tdot";                        // Datenbankname
+$port = 3306;                            // MySQL-Standardport
 
-// Pfad zum SSL-Zertifikat
+// Verbindung herstellen
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Initialisiere MySQLi
-$conn = mysqli_init();
-
-// SSL-Einstellungen setzen
-
-// Verbinde ohne Zertifikatsprüfung
-if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
-    die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
+// Verbindung auf Fehler prüfen
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
+
 ?>
